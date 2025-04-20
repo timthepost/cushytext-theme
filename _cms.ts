647b3e686c3860e6a8c6e13786f8058b9113cc96
= <=>
2025-04-19 20:58:41 -0400

    chore: refresh cms config

--- _cms.ts
+++ _cms.ts
@@ -2,14 +2,8 @@ import lumeCMS from "lume/cms/mod.ts";
 
 const cms = lumeCMS({
   site: {
-    name: "Cushy Text",
-    description: "This is the back-end to Cushy Text",
-    body: `
-      <h2>Post Feedback Management:</h2>
-      <p>
-        We need a way to view feedback and other DenoKV data.
-      </p>
-    `,
+    name: "Your Site",
+    description: "Your Description",
   },
 });
 
@@ -27,7 +21,7 @@ cms.collection("docs", "src:docs/*.mdx", [
   "content: markdown",
 ]);
 
-cms.collection("pages", "src:features/*.mdx", [
+cms.collection("pages", "src:pages/*.mdx", [
   "title: text",
   "content: markdown",
 ]);
